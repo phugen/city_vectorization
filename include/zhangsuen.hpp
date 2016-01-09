6645356059b142e388736d32667cbc2f8113af59
Philipp Hugenroth <p_huge01@uni-muenster.de>
2016-01-09 16:35:30 +0100

    Missing zhangsuen.hpp added.

---
+++ include/zhangsuen.hpp
@@ -0,0 +1,10 @@
+#ifndef ZHANGSUEN_HPP
+#define ZHANGSUEN_HPP
+
+#include "include/opencvincludes.hpp"
+
+void thinningIteration(cv::Mat& im, int iter);
+void thinning(cv::Mat& im);
+
+#endif // ZHANGSUEN_HPP
+
